81aa025ec9fa57b8f1f0dc3b27473f987108164d
Alexandr <aleksandr-trutnev@mail.ru>
2025-03-17 05:20:16 +0300

    Create scripts

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Drag : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                isDragging = true;
+                offset = transform.position - mousePosition;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        if (isDragging)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            transform.position = mousePosition + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
